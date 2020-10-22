8515978edf0bdf2edf2325c5f14c8571affdc9de
icarolettieri <icaro_lettieri@live.com>
2020-10-22 19:00:47 -0300

    conceitos typescript

---
+++ nivel01/typescript/nodejs/src/routes.ts
@@ -0,0 +1,26 @@
+import { response } from "express";
+
+import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+/**
+ * string, number, boolean, object, Array
+ * interfaces
+ */
+
+export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'diego@rocketseat.com.br',
+    password: '123456',
+    techs: [
+      'Node.js', 
+      'ReactJS', 
+      'React Native',
+      { title: 'Javascrip', experience: 100 },
+    ],
+  });
+
+  console.log(user.)
+
+  return response.json({ message: 'Hello GoStack' });
+}
